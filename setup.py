c822f24fc3d6c8c93a0eeb138e095d3fc9c74c7a
Luke Barbier <lukebarbier12@gmail.com>
2020-07-20 07:44:01 -0600

    python2 support

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['ping360_sonar'],
+     packages=['ping360_sonar', 'brping'],
      package_dir={'': 'src'},
      install_requires=['bluerobotics-ping']
 )
